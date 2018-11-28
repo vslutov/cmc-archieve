03a50304ff9a9c39385962459e62df9173490fa8
vslutov <vslutov@yandex.ru>
2018-11-28 23:44:35 +0300

    Add tasks

--- EleventhSemester/hadoop/task02/start.sh
+++ EleventhSemester/hadoop/task02/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in {5..40..5}
+for i in {5..80..5}
 do
   hdfs dfs -rm -r output.csv
   /usr/bin/time -o "result.$i.txt" -f '%E' spark-submit correlation.py "wasb://rts@bigdatasets.blob.core.windows.net/deals" output.csv --securities='(SVH1)|(EDH1)' --date_from=20110111 --date_to=20110112 --time_from=1000 --time_to=1800 --widths=1,5,10 --shifts=0,1,2,3,4,5 --num_reducers="$i"
