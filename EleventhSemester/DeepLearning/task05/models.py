1f4567d2de1c05b4dda724597f5e3fbdb13c8d87
vslutov <vslutov@yandex.ru>
2018-12-10 22:49:08 +0300

    Add deep learning tasks

--- EleventhSemester/DeepLearning/task05/models.py
+++ EleventhSemester/DeepLearning/task05/models.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 import math
 
 """
@@ -71,6 +72,7 @@ class AE(nn.Module):
         representation = torch.normal(torch.zeros(num_samples, self.d), torch.ones(num_samples, self.d)).to(device)
         return self.decode(representation).cpu()
 
+EPS = 1e-6
 
 def log_mean_exp(data):
     """
@@ -79,7 +81,7 @@ def log_mean_exp(data):
     Вход: mtx, Tensor - тензор размера n_1 x n_2 x ... x n_K.
     Возвращаемое значение: Tensor, тензор размера n_1 x n_2 x ,,, x n_{K - 1}.
     """
-    torch.logsumexp(data, -1)
+    return torch.logsumexp(data, -1) - math.log(data.shape[-1])
 
 def log_likelihood(x_true, x_distr):
     """
@@ -97,11 +99,13 @@ def log_likelihood(x_true, x_distr):
     Выход: Tensor, матрица размера n x K - оценки логарифма правдоподобия
     каждого сэмпла.
     """
-    n, K, D = x_distr.shape()
+    n, K, D = x_distr.shape
+    x_distr = x_distr.clamp(EPS, 1 - EPS)
     x_log_dist = x_distr.log()
-    return ((x_log_dist * x_true.view(n, 1, D)) + (x_log_dist * (1 - x_true).view(n, 1, D))).sum(1)
+    m_x_log_dist = (1 - x_distr).log()
+    return ((x_log_dist * x_true.view(n, 1, D)) + (m_x_log_dist * (1 - x_true).view(n, 1, D))).sum(2)
 
-def kl(q_distr, p_distr):
+def kl(p_distr, q_distr):
     """
     Вычисляется KL-дивергенция KL(q || p) между n парами гауссиан.
     Вход: q_distr, tuple(Tensor, Tensor). Каждый Tensor - матрица размера n x d.
@@ -112,9 +116,11 @@ def kl(q_distr, p_distr):
     """
     p_mu, p_sigma = p_distr
     q_mu, q_sigma = q_distr
-    # ваш код здесь
-    pass
-
+    return (q_sigma.log()
+            - p_sigma.log()
+            + (p_sigma ** 2 + (p_mu - q_mu) ** 2) / (2 * q_sigma ** 2)
+            - 0.5
+           ).sum(1)
 
 class VAE(nn.Module):
     def __init__(self, d, D):
@@ -150,8 +156,10 @@ class VAE(nn.Module):
         Каждый Tensor - матрица размера n x d.
         Первый - mu, второй - sigma.
         """
-        # ваш код здесь
-        pass
+        device = next(iter(self.parameters())).device
+        proposal = self.proposal_network(x.to(device))
+        mu = self.proposal_mu_head(proposal)
+        sigma = F.softplus(self.proposal_sigma_head(proposal))
         return mu, sigma
 
     def prior_distr(self, n):
@@ -162,7 +170,9 @@ class VAE(nn.Module):
         Каждый Tensor - матрица размера n x d.
         Первый - mu, второй - sigma.
         """
-        # ваш код здесь
+        device = next(iter(self.parameters())).device
+        mu = torch.zeros(n, self.d, device=device)
+        sigma = torch.ones(n, self.d, device=device)
         return mu, sigma
 
     def sample_latent(self, distr, K=1):
@@ -174,8 +184,11 @@ class VAE(nn.Module):
         Вход: K, int - число сэмплов для каждого объекта.
         Возвращаемое значение: Tensor, матрица размера n x K x d.
         """
-        # ваш код здесь
-        pass
+        mu, sigma = distr
+        device = mu.device
+        n, d = mu.shape
+        distr = torch.normal(torch.zeros(n, K, d, device=device), torch.ones(n, K, d, device=device)) 
+        return distr * sigma.view(n, 1, d) + mu.view(n, 1, d)
 
     def generative_distr(self, z):
         """
@@ -185,10 +198,12 @@ class VAE(nn.Module):
         Возвращаемое значение: Tensor, тензор параметров распределения
         Бернулли размера n x K x D.
         """
-        # ваш код здесь
-        pass
+        device = next(iter(self.parameters())).device
+        n, K, d = z.shape
+        result = self.generative_network(z.to(device).view(-1, d)).view(n, K, -1)
+        return result
 
-    def batch_loss(self, batch):
+    def batch_loss(self, x_true):
         """
         Вычисляет вариационную нижнюю оценку логарифма правдоподобия по батчу.
         Вариационная нижняя оценка должна быть дифференцируема по параметрам модели (!),
@@ -198,8 +213,13 @@ class VAE(nn.Module):
         Возвращаемое значение: Tensor, скаляр - вариационная нижняя оценка логарифма
         правдоподобия по батчу.
         """
-        # ваш код здесь
-        pass
+        mu, sigma = self.proposal_distr(x_true)
+        z = self.sample_latent((mu, sigma))
+        x_distr = self.generative_distr(z)
+        reconstructed_loss = log_likelihood(x_true, x_distr).sum(1).mean(0)
+        kl_div = kl((mu, sigma), self.prior_distr(x_true.shape[0])).mean()
+        return reconstructed_loss - kl_div
+        
 
     def generate_samples(self, num_samples):
         """
@@ -207,8 +227,10 @@ class VAE(nn.Module):
         Вход: num_samples, int - число сэмплов, которые надо сгененрировать.
         Возвращаемое значение: Tensor, матрица размера num_samples x D.
         """
-        # ваш код здесь
-        pass
+        mu, sigma = self.prior_distr(num_samples)
+        z = self.sample_latent((mu, sigma))
+        sample = self.generative_distr(z)
+        return torch.bernoulli(sample.view(num_samples, self.D)).cpu()
 
 
 def gaussian_log_pdf(distr, samples):
@@ -222,10 +244,13 @@ def gaussian_log_pdf(distr, samples):
     плотности вероятности точки относительно соответствующего распределения.
     """
     mu, sigma = distr
-    # ваш код здесь
+    n, K, d = samples.shape
+    sqrt_2_pi = math.log(math.sqrt(2 * math.pi))
+    log_pdf = - sqrt_2_pi - sigma.log().view(n, 1, d) - (samples - mu.view(n, 1, d)) ** 2 / (2 * sigma ** 2).view(n, 1, d)
+    return log_pdf.sum(-1)
 
 
-def compute_log_likelihood_monte_carlo(batch, model, K):
+def compute_log_likelihood_monte_carlo(x_true, model, K):
     """
     Функция, оценку логарифма правдоподобия вероятностной модели по батчу методом Монте-Карло.
     Оценка логарифма правдоподобия модели должна быть усреднена по всем объектам батча.
@@ -238,11 +263,13 @@ def compute_log_likelihood_monte_carlo(batch, model, K):
     Вход: K, int - количество сэмплов.
     Возвращаемое значение: float - оценка логарифма правдоподобия.
     """
-    # ваш код здесь
-    pass
+    priori = model.prior_distr(x_true.shape[0])
+    z = model.sample_latent(priori, K)
+    x_distr = model.generative_distr(z)
+    return log_mean_exp(log_likelihood(x_true, x_distr)).mean().item()
 
 
-def compute_log_likelihood_iwae(batch, model, K):
+def compute_log_likelihood_iwae(x_true, model, K):
     """
     Функция, оценку IWAE логарифма правдоподобия вероятностной модели по батчу.
     Оценка логарифма правдоподобия модели должна быть усреднена по всем объектам батча.
@@ -255,5 +282,16 @@ def compute_log_likelihood_iwae(batch, model, K):
     Вход: K, int - количество сэмплов.
     Возвращаемое значение: float - оценка логарифма правдоподобия.
     """
-    # ваш код здесь
-    pass
+    n, D = x_true.shape
+    mu, sigma = model.proposal_distr(x_true)
+    priori = model.prior_distr(x_true.shape[0])
+
+    z = model.sample_latent((mu, sigma), K)
+    x_distr = model.generative_distr(z)
+    
+    p_x_z = log_likelihood(x_true, x_distr)
+    p_z = gaussian_log_pdf(priori, z)
+    q_z_x = gaussian_log_pdf((mu, sigma), z)
+    
+    p = (p_x_z + p_z - q_z_x)
+    return log_mean_exp(p).mean().item()
