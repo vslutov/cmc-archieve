9e45c8414a870148a01bd6a584aab604560d1a92
vslutov <vslutov@yandex.ru>
2018-12-19 22:42:22 +0300

    complete polus result

--- EleventhSemester/hypercomputing/task02/start-polus.sh
+++ EleventhSemester/hypercomputing/task02/start-polus.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 make -f Makefile.mpi-polus
 
-for p in 128 256 512 125 216
+for p in 1 2 4 8 16 32 27 48
 do
-  for m in 512 1024 1536
+  for m in 128 256 512
   do
     mpisubmit.pl -p $p parmpi.$m
   done
