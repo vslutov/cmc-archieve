692a1b25b9062a2c79e22ee4133e80124c512141
vslutov <vslutov@yandex.ru>
2019-01-04 20:33:23 +0300

    finish part 1

--- EleventhSemester/cplusplus/task03/part1.cpp
+++ EleventhSemester/cplusplus/task03/part1.cpp
@@ -1,37 +1,56 @@
 #include <iostream>
 #include <thread>
 #include <queue>
-#include <atomic>
 #include <mutex>
+#include <condition_variable>
 
 int main() {
-  std::atomic<size_t> count = 0;
-  std::atomic<bool> done = false;
+  size_t count = 0;
+  bool done = false;
   std::queue<int> items;
   std::mutex m;
+  std::condition_variable cv;
+
   std::thread producer([&]() {
     for (int i = 0; i < 10000; ++i) {
       // ... some code may be here ...
       {
         std::lock_guard<std::mutex> lock(m);
         items.push(i);
+        ++count;
+        cv.notify_one();
       }
-      ++count;
     }
-    done = true;
+
+    {
+      std::lock_guard<std::mutex> lock(m);
+      done = true;
+      cv.notify_one();
+    }
   });
+
   std::thread consumer([&]() {
-    while (!done.load()) {
+    std::unique_lock<std::mutex> lock(m);
+
+    while (true) {
+      cv.wait(lock, [&]() {
+        return !items.empty() || done;
+      });
+
       while (!items.empty()) {
-        {
-          std::lock_guard<std::mutex> lock(m);
-          items.pop();
-        }
+        items.pop();
+        lock.unlock();
         // ...
+        lock.lock();
         --count;
       }
+
+      if (done) {
+        break;
+      }
     }
   });
+
   producer.join();
   consumer.join();
   std::cout << count << std::endl;
