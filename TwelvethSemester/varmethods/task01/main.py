11696672b7982466778c0277fc39a586d6666d8f
vslutov <vslutov@yandex.ru>
2019-04-20 22:29:44 +0300

    update varmethod task 01

--- TwelvethSemester/varmethods/task01/main.py
+++ TwelvethSemester/varmethods/task01/main.py
@@ -5,7 +5,7 @@ import argparse
 
 from skimage.io import imread, imsave
 from skimage import img_as_float32, img_as_ubyte
-from skimage.color import gray2rgb, rgb2gray, rgb2ycbcr, ycbcr2rgb
+from skimage.color import gray2rgb, rgb2gray
 import numpy as np
 from scipy.signal import convolve2d
 from tqdm.autonotebook import tqdm
@@ -41,55 +41,51 @@ class ZeroIsALie:
     def __setitem__(self, indeces, value):
         return self.array.__setitem__(zero_is_a_lie(indeces), value)
 
-Q = ((0, 1), (1, 0), (1, 1))
+Q = np.array(((0, 1), (1, 0), (1, 1)))
+Q_coef = 1 / np.sqrt((Q ** 2).sum(axis=1))
 
 def btv(image):
-    value = 0
     grad = ZeroIsALie(np.zeros_like(image))
     image = ZeroIsALie(image)
 
-    for i, j in Q:
-        diff = image[i:, j:] - image[:-i, :-j]
-        value += np.abs(diff).sum() / (len(Q) * np.sqrt(i ** 2 + j ** 2))
+    for (i, j), c in zip(Q, Q_coef):
+        diff = image[i:, j:] - image[:-i, :-j] * c
         grad[i:, j:] += np.sign(diff)
         grad[:-i, :-j] -= np.sign(diff)
 
-    return value, grad.array
+    return grad.array
 
 def btv2(image):
-    value = 0
     grad = ZeroIsALie(np.zeros_like(image))
     image = ZeroIsALie(image)
 
-    for i, j in Q:
-        diff = image[2 * i:, 2 * j:] + image[:-2 * i, :-2 * j] - 2 * image[i:-i, j:-j]
-        value += np.abs(diff).sum() / (len(Q) * np.sqrt(i ** 2 + j ** 2))
+    for (i, j), c in zip(Q, Q_coef):
+        diff = image[2 * i:, 2 * j:] + image[:-2 * i, :-2 * j] - 2 * image[i:-i, j:-j] * c
         grad[2 * i:, 2 * j:] += np.sign(diff)
         grad[:-2 * i, :-2 * j] += np.sign(diff)
         grad[i:-i, j:-j] -= 2 * np.sign(diff)
 
-    return value, grad.array
+    return grad.array
 
-def tgv(image):
-    btv_value, btv_grad = btv(image)
-    btv2_value, btv2_grad = btv2(image)
-    btv_coef, btv2_coef = 0.01, 0.01
+def tgv(image, noise_level):
+    btv_grad = btv(image)
+    btv2_grad = btv2(image)
+    btv2_coef = btv_coef = 1e-4 + 19e-4 * noise_level / 20
 
-    return btv_value * btv_coef + btv2_value * btv2_coef, btv_grad * btv_coef + btv2_grad * btv2_coef
+    return btv_grad * btv_coef + btv2_grad * btv2_coef
 
 def dist(target, image, kernel):
     N, M, C = target.shape
     K, L = kernel.shape
 
     diff = convolve(target, kernel) - image
-    dist_value = np.abs(diff).sum()
 
     dist_grad = np.zeros_like(target)
     for k in range(K):
         for l in range(L):
             dist_grad[k:k + N - K + 1, l:l + M - L + 1] += kernel[k, l] * diff
 
-    return dist_value, dist_grad
+    return dist_grad
 
 def deconvolve(image, kernel, noise_level):
     N, M, C = image.shape
@@ -97,47 +93,44 @@ def deconvolve(image, kernel, noise_level):
     N += K - 1
     M += L - 1
 
-    result = target = rgb2ycbcr(np.zeros((N, M, C)).astype(image.dtype) + 0.5)
-    # target[K // 2:K // 2 + image.shape[0], L // 2:L // 2 + image.shape[1], 0] = image[:, :, 0]
-    momentum = np.zeros_like(result)
+    target = (np.zeros((N, M, C)).astype(image.dtype) + 0.5)
+    momentum = np.zeros_like(target)
     best_loss = float('+inf')
 
-    for i in tqdm(range(40), desc='iteration'):
-        lr = 0.25 * 0.01 ** ((i + 1) / 40.0)
+    for i in tqdm(range(100), desc='SGD'):
+        lr = 0.25 * 0.01 ** (i / 100.0)
 
-        nesterov_target = target + momentum
+        nesterov_target = target + 0.8 * momentum
 
-        dist_value, dist_grad = dist(nesterov_target, image, kernel)
-        tgv_value, tgv_grad = tgv(nesterov_target)
+        dist_grad = dist(nesterov_target, image, kernel)
+        tgv_grad = tgv(nesterov_target, noise_level)
 
-        loss = dist_value + tgv_value
         grad_target = dist_grad + tgv_grad
 
-        momentum = 0.85 * momentum - lr * grad_target
+        momentum = - lr * grad_target + 0.8 * momentum
         target += momentum
 
-        if loss < best_loss:
-            result = target
-
-        imsave('tmp.png', img_as_ubyte(ycbcr2rgb(result).clip(0, 1)))
-
-    return result
+    return target
 
 
 def denoise(args):
     kernel = img_as_float32(imread(args.kernel))
+    if kernel.shape[2] == 4:
+        kernel = kernel[:, :, :3]
     if len(kernel.shape) == 3:
         kernel = rgb2gray(kernel)
     kernel /= kernel.sum()
 
     image = img_as_float32(imread(args.input_image))
+    if image.shape[2] == 4:
+        image = image[:, :, :3]
     if len(image.shape) == 2:
         image = gray2rgb(image)
-    image = rgb2ycbcr(image)
+    image = (image)
 
     result = deconvolve(image, kernel, args.noise_level)
 
-    imsave(args.output_image, img_as_ubyte(ycbcr2rgb(result).clip(0, 1)))
+    imsave(args.output_image, img_as_ubyte((result).clip(0, 1)))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -145,7 +138,7 @@ def main():
     parser.add_argument('input_image')
     parser.add_argument('kernel')
     parser.add_argument('output_image')
-    parser.add_argument('noise_level', type=int)
+    parser.add_argument('noise_level', type=float)
 
     args = parser.parse_args()
 
