b13a9735c0f68e853de342d8110d297b87ccb473
vslutov <vslutov@yandex.ru>
2019-04-24 22:01:32 +0300

    finish gpu task01

--- TwelvethSemester/cuda/task01/GPUTask01.cu
+++ TwelvethSemester/cuda/task01/GPUTask01.cu
@@ -10,6 +10,7 @@
 #include <cuda_runtime.h>
 
 #include <SOIL/SOIL.h>
+#include "helper_math.h"
 
 const float EPS = 0.15 * 0.15;
 const ssize_t BOX_SIZE = 5;
@@ -22,6 +23,11 @@ const ssize_t SHARED_BUFFER_SIZE_Y = BLOCK_SIZE_Y + BOX_SIZE - 1;
 
 // Access to array
 #define ACCESS(arr, x, y, c, w, h, channels) (arr[((y) * w + (x)) * (channels) + (c)])
+#define ACCESS_TEXTURE(arr, x, y, w, h) tex2D(arr, float(x) / w, float(y) / h)
+
+texture<float4, 2, cudaReadModeElementType> tex;
+texture<float4, 2, cudaReadModeElementType> texA;
+texture<float4, 2, cudaReadModeElementType> texB;
 
 // Check that point is outside of array
 __host__ __device__ int
@@ -30,7 +36,7 @@ validAddress(int x, int y, int w, int h)
   return x >= 0 && x < w && y >= 0 && y < h;
 }
 
-inline void
+__host__ __device__ inline void
 calcTmp (
   uint8_t *inImg,
   float *tmpImg,
@@ -67,6 +73,43 @@ calcTmp (
     ACCESS(tmpImg, x, y, 2 * c, w, h, 6) = a;
     ACCESS(tmpImg, x, y, 2 * c + 1, w, h, 6) = b;
   }
+}
+
+__device__ inline void
+calcTmpTexture (
+  float4 *tmpA,
+  float4 *tmpB,
+  ssize_t x,
+  ssize_t y,
+  ssize_t w,
+  ssize_t h,
+  float eps
+)
+{
+  float4 mean = make_float4(0, 0, 0, 0);
+  float4 corr = make_float4(0, 0, 0, 0);
+  ssize_t count = 0;
+
+  for (ssize_t i = x - PAD_SIZE; i < x + BOX_SIZE - PAD_SIZE; ++ i) {
+    for (ssize_t j = y - PAD_SIZE; j < y + BOX_SIZE - PAD_SIZE; ++ j) {
+      if (validAddress(i, j, w, h)) {
+        float4 color = ACCESS_TEXTURE(tex, i, j, w, h) / 255.0;
+        mean += color;
+        corr += color * color;
+        ++ count;
+      }
+    }
+  }
+
+  mean /= count;
+  corr /= count;
+  float4 var = corr - mean * mean;
+
+  float4 a = var / (var + eps);
+  float4 b = (make_float4(1, 1, 1, 1) - a) * mean;
+
+  ACCESS(tmpA, x, y, 0, w, h, 1) = a;
+  ACCESS(tmpB, x, y, 0, w, h, 1) = b;
 }
 
 __device__ void
@@ -187,7 +230,7 @@ prepareSharedBufferFromTmp(
   __syncthreads();
 }
 
-inline  void
+__host__ __device__ inline  void
 calcOut(
   uint8_t *inImg,
   float *tmpImg,
@@ -218,6 +261,38 @@ calcOut(
 
     ACCESS(outImg, x, y, c, w, h, 3) = ACCESS(inImg, x, y, c, w, h, 3) * meanA + 255 * meanB;
   }
+}
+
+__device__ inline  void
+calcOutTexture(
+  uint8_t *outImg,
+  ssize_t x,
+  ssize_t y,
+  ssize_t w,
+  ssize_t h
+)
+{
+  float4 meanA = make_float4(0, 0, 0, 0);
+  float4 meanB = make_float4(0, 0, 0, 0);
+  ssize_t count = 0;
+
+  for (ssize_t i = x - PAD_SIZE; i < x + BOX_SIZE - PAD_SIZE; ++ i) {
+    for (ssize_t j = y - PAD_SIZE; j < y + BOX_SIZE - PAD_SIZE; ++ j) {
+      if (validAddress(i, j, w, h)) {
+        meanA = meanA + ACCESS_TEXTURE(texA, i, j, w, h);
+        meanB = meanB + ACCESS_TEXTURE(texB, i, j, w, h);
+        ++ count;
+      }
+    }
+  }
+
+  meanA /= count;
+  meanB /= count;
+
+  float4 result = ACCESS_TEXTURE(tex, x, y, w, h) * meanA + 255 * meanB;
+  ACCESS(outImg, x, y, 0, w, h, 3) = result.x;
+  ACCESS(outImg, x, y, 1, w, h, 3) = result.y;
+  ACCESS(outImg, x, y, 2, w, h, 3) = result.z;
 }
 
 __device__  void
@@ -303,7 +378,87 @@ guidedOpenMP(
 }
 
 __global__ void
-guidedGpuTmp(
+guidedGpuTmpGlobal(
+  uint8_t *inImg,
+  float *tmpImg,
+  ssize_t w,
+  ssize_t h,
+  float eps
+)
+{
+  // compute idx
+  int x = threadIdx.x + blockIdx.x * blockDim.x;
+  int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (!validAddress(x, y, w, h)) {
+    return;
+  }
+
+  calcTmp(inImg, tmpImg, x, y, w, h, eps);
+}
+
+__global__ void
+guidedGpuTmpTexture(
+  float4 *tmpA,
+  float4 *tmpB,
+  ssize_t w,
+  ssize_t h,
+  float eps
+)
+{
+  // compute idx
+  int x = threadIdx.x + blockIdx.x * blockDim.x;
+  int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (!validAddress(x, y, w, h)) {
+    return;
+  }
+
+  calcTmpTexture(tmpA, tmpB, x, y, w, h, eps);
+}
+
+__global__ void
+guidedGpuOutGlobal(
+  uint8_t *inImg,
+  float *tmpImg,
+  uint8_t *outImg,
+  ssize_t w,
+  ssize_t h,
+  float eps
+)
+{
+  // compute idx
+  int x = threadIdx.x + blockIdx.x * blockDim.x;
+  int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (!validAddress(x, y, w, h)) {
+    return;
+  }
+
+  calcOut(inImg, tmpImg, outImg, x, y, w, h);
+}
+
+__global__ void
+guidedGpuOutTexture(
+  uint8_t *outImg,
+  ssize_t w,
+  ssize_t h,
+  float eps
+)
+{
+  // compute idx
+  int x = threadIdx.x + blockIdx.x * blockDim.x;
+  int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (!validAddress(x, y, w, h)) {
+    return;
+  }
+
+  calcOutTexture(outImg, x, y, w, h);
+}
+
+__global__ void
+guidedGpuTmpShared(
   uint8_t *inImg,
   float *tmpImg,
   ssize_t w,
@@ -323,7 +478,7 @@ guidedGpuTmp(
 }
 
 __global__ void
-guidedGpuOut(
+guidedGpuOutShared(
   uint8_t *inImg,
   float *tmpImg,
   uint8_t *outImg,
@@ -353,7 +508,7 @@ checkErrors()
 }
 
 void
-guidedGpu(
+guidedGpuShared(
   uint8_t *inImg,
   float *tmpImg,
   uint8_t *outImg,
@@ -365,8 +520,59 @@ guidedGpu(
   dim3 gridSize(std::ceil((float) w / BLOCK_SIZE_X),
                 std::ceil((float) h / BLOCK_SIZE_Y));
 
-  guidedGpuTmp<<<gridSize, BLOCK_SIZE>>>(inImg, tmpImg, w, h, eps); checkErrors();
-  guidedGpuOut<<<gridSize, BLOCK_SIZE>>>(inImg, tmpImg, outImg, w, h, eps); checkErrors();
+  guidedGpuTmpShared<<<gridSize, BLOCK_SIZE>>>(inImg, tmpImg, w, h, eps); checkErrors();
+  guidedGpuOutShared<<<gridSize, BLOCK_SIZE>>>(inImg, tmpImg, outImg, w, h, eps); checkErrors();
+}
+
+void
+guidedGpuGlobal(
+  uint8_t *inImg,
+  float *tmpImg,
+  uint8_t *outImg,
+  ssize_t w,
+  ssize_t h,
+  float eps
+)
+{
+  dim3 gridSize(std::ceil((float) w / BLOCK_SIZE_X),
+                std::ceil((float) h / BLOCK_SIZE_Y));
+
+  guidedGpuTmpGlobal<<<gridSize, BLOCK_SIZE>>>(inImg, tmpImg, w, h, eps); checkErrors();
+  guidedGpuOutGlobal<<<gridSize, BLOCK_SIZE>>>(inImg, tmpImg, outImg, w, h, eps); checkErrors();
+}
+
+void
+guidedGpuTexture(
+  float4 *tmpA,
+  float4 *tmpB,
+  cudaArray *cuArrayA,
+  cudaArray *cuArrayB,
+  uint8_t *outImg,
+  ssize_t w,
+  ssize_t h,
+  float eps
+)
+{
+  dim3 gridSize(std::ceil((float) w / BLOCK_SIZE_X),
+                std::ceil((float) h / BLOCK_SIZE_Y));
+
+  guidedGpuTmpTexture<<<gridSize, BLOCK_SIZE>>>(tmpA, tmpB, w, h, eps); checkErrors();
+
+  cudaMemcpyToArray(cuArrayA,
+                    0,
+                    0,
+                    tmpA,
+                    w * h * sizeof(float4),
+                    cudaMemcpyDeviceToDevice); checkErrors();
+
+  cudaMemcpyToArray(cuArrayB,
+                    0,
+                    0,
+                    tmpB,
+                    w * h * sizeof(float4),
+                    cudaMemcpyDeviceToDevice); checkErrors();
+
+  guidedGpuOutTexture<<<gridSize, BLOCK_SIZE>>>(outImg, w, h, eps); checkErrors();
 }
 
 float
@@ -434,15 +640,76 @@ main(int argc, char **argv)
   cudaMalloc((void **) &tmpImgDev, sizeof(float) * 2 * image_size); checkErrors();
   cudaMemcpy(inImgDev, inImg, image_size, cudaMemcpyHostToDevice); checkErrors();
 
+  // Allocate array and copy image data
+  auto hDataContainer = std::unique_ptr<float4[]>(new float4[width * height]);
+  auto hData = hDataContainer.get();
+  for (ssize_t i = 0; i < width * height; ++ i) {
+    hData[i] = make_float4(inImg[i * 3], inImg[i * 3 + 1], inImg[i * 3 + 2], 0);
+  }
+  auto channelDesc = cudaCreateChannelDesc<float4>();
+  cudaArray *cuArray;
+  cudaMallocArray(&cuArray,
+                  &channelDesc,
+                  width,
+                  height); checkErrors();
+  cudaMemcpyToArray(cuArray,
+                    0,
+                    0,
+                    hData,
+                    width * height * sizeof(float4),
+                    cudaMemcpyHostToDevice); checkErrors();
+
+  tex.addressMode[0] = cudaAddressModeWrap;
+  tex.addressMode[1] = cudaAddressModeWrap;
+  tex.filterMode = cudaFilterModeLinear;
+  tex.normalized = true;    // access with normalized texture coordinates
+  cudaBindTextureToArray(tex, cuArray, channelDesc); checkErrors();
+
+  float4 *tmpA;
+  cudaMalloc((void **) &tmpA, sizeof(float4) * width * height); checkErrors();
+  cudaArray *cuArrayA;
+  cudaMallocArray(&cuArrayA,
+                  &channelDesc,
+                  width,
+                  height); checkErrors();
+  texA.addressMode[0] = cudaAddressModeWrap;
+  texA.addressMode[1] = cudaAddressModeWrap;
+  texA.filterMode = cudaFilterModeLinear;
+  texA.normalized = true;    // access with normalized texture coordinates
+  cudaBindTextureToArray(texA, cuArrayA, channelDesc); checkErrors();
+
+  float4 *tmpB;
+  cudaMalloc((void **) &tmpB, sizeof(float4) * width * height); checkErrors();
+  cudaArray *cuArrayB;
+  cudaMallocArray(&cuArrayB,
+                  &channelDesc,
+                  width,
+                  height); checkErrors();
+  texB.addressMode[0] = cudaAddressModeWrap;
+  texB.addressMode[1] = cudaAddressModeWrap;
+  texB.filterMode = cudaFilterModeLinear;
+  texB.normalized = true;    // access with normalized texture coordinates
+  cudaBindTextureToArray(texB, cuArrayB, channelDesc); checkErrors();
+
   // cuda
-  const auto timeWithCopy = measureTime("CUDA with copy", [inImgDev, tmpImgDev, outImgDev, width, height, inImg, outImg, image_size]() {
+  const auto timeWithCopy = measureTime("CUDA shared with copy", [inImgDev, tmpImgDev, outImgDev, width, height, inImg, outImg, image_size]() {
     cudaMemcpy(inImgDev, inImg, image_size, cudaMemcpyHostToDevice); checkErrors();
-    guidedGpu(inImgDev, tmpImgDev, outImgDev, width, height, EPS);
+    guidedGpuShared(inImgDev, tmpImgDev, outImgDev, width, height, EPS);
     cudaMemcpy(outImg, outImgDev, image_size, cudaMemcpyDeviceToHost); checkErrors();
   });
 
-  const auto timeWithoutCopy = measureTime("CUDA without copy", [inImgDev, tmpImgDev, outImgDev, width, height]() {
-    guidedGpu(inImgDev, tmpImgDev, outImgDev, width, height, EPS);
+  const auto timeWithoutCopy = measureTime("CUDA shared without copy", [inImgDev, tmpImgDev, outImgDev, width, height]() {
+    guidedGpuShared(inImgDev, tmpImgDev, outImgDev, width, height, EPS);
+    cudaDeviceSynchronize(); checkErrors();
+  });
+
+  measureTime("CUDA global without copy", [inImgDev, tmpImgDev, outImgDev, width, height]() {
+    guidedGpuGlobal(inImgDev, tmpImgDev, outImgDev, width, height, EPS);
+    cudaDeviceSynchronize(); checkErrors();
+  });
+
+  measureTime("CUDA texture without copy", [tmpA, tmpB, cuArrayA, cuArrayB, outImgDev, width, height]() {
+    guidedGpuTexture(tmpA, tmpB, cuArrayA, cuArrayB, outImgDev, width, height, EPS);
     cudaDeviceSynchronize(); checkErrors();
   });
 
